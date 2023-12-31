0d10cc9ae0b5b63c4107018a3d2ad13c6ba4bc49
SuguruOoki <ohki_suguru@sue-san.dev>
2023-12-31 17:12:01 +0900

    modify: index.phpにpdf-parserの処理を追加

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-<?
-phpinfo();
+<?php
+
+require 'vendor/autoload.php';
+
+$parser    = new \Smalot\PdfParser\Parser();
+$pdf       = $parser->parseFile('pdf/sample.pdf');
+$firstPage = $pdf->getPages()[0];
+
+echo nl2br($firstPage->getText());
